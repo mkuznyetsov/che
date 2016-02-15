52fe2a2d6ef94c66e14a41a20e7d80be470e56ff
Max Shaposhnik <mshaposhnik@codenvy.com>
2016-02-15 15:24:58 +0200

    CLDIDE-2693 unauthorized exception contain information for auth
    Signed-off-by: Max Shaposhnik <mshaposhnik@codenvy.com>

---
+++ core/platform-api/che-core-api-git/src/main/java/org/eclipse/che/api/git/ProviderInfo.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.api.git;
+
+/**
+ * Contains additional info about the credential provider, such as provider id and
+ * URL that can be used to authorize in order to perform operations.
+ *
+ * @author Max Shaposhnik
+ */
+public class ProviderInfo {
+
+    private String authenticateUrl;
+
+    private String providerId;
+
+    public ProviderInfo(String authenticateUrl, String providerId) {
+        this.authenticateUrl = authenticateUrl;
+        this.providerId = providerId;
+    }
+
+    public String getProviderId() {
+
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+
+    public String getAuthenticateUrl() {
+        return authenticateUrl;
+    }
+
+    public void setAuthenticateUrl(String authenticateUrl) {
+        this.authenticateUrl = authenticateUrl;
+    }
+
+}
